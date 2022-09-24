c6cd5e60545a50769b3746412b5197a1f2b886a9
Obrienjames34 <Obrienjames934@gmail.com>
2022-09-24 17:47:14 -0400

    src

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/Nav";
 
 function App() {
-  return <Nav />;
+  const [currentPage, setCurrentPage] = useState(
+    "Im the Goat Coder of all time!"
+  );
+  return <Nav setCurrentPage={setCurrentPage} currentPage={currentPage} />;
+  <div className="app-container">
+    <Nav currentPage={currentPage} setCurrentPage={setCurrentPage} />
+    <main className="main-container">
+      {currentPage === "about" && <About />}
+      {currentPage === "portfolio" && <Portfolio />}
+      {currentPage === "resume" && <Resume />}
+      {currentPage === "contact" && <Contact />}
+    </main>
+    <Footer />
+  </div>;
 }
 
 export default App;
